e0997b2ac0d717eb07c018ec0122dbbcd6998cdd
AfroComrade <72281933+AfroComrade@users.noreply.github.com>
2022-10-10 10:19:59 +1300

    Adding cors to the backend

--- SPEED/backend/server.js
+++ SPEED/backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const cors = require('cors');
 const port = process.env.PORT || 8082;
 
 // routes
@@ -14,6 +15,7 @@ connectDB();
 
 // Init Middleware
 app.use(express.json({ extended: false }));
+app.use(cors({ origin: true, credentials: true }));
 
 app.get('/', (req, res) => res.send('Hello world!'));
 
